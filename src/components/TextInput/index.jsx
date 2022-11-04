54bb732614b464a4cbfa91140c53ebf4c27cb209
Leo Graciano <leonardoferreiragraciano@gmail.com>
2022-11-04 09:28:51 -0300

    initial

---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,13 @@
+import './styles.sass';
+
+export const TextInput = ({ searchValue, handleChange }) => {
+  return (
+    <input
+      className="text-input"
+      onChange={handleChange}
+      value={searchValue}
+      type="search"
+      placeholder="Type your search"
+    />
+  );
+}
